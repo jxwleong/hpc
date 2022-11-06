bf5ec340b1c0d09b2cd7c25f1200c2a5699d19c7
jxwleong <jason.xie.wei.leong@gmail.com>
2022-11-06 12:45:11 +0800

    Added code for Intersperse method

--- lab2_summation_calculation/summation.c
+++ lab2_summation_calculation/summation.c
@@ -10,7 +10,7 @@ printf("The sum of the reciprocal from 1 to %d is %f.\n", n, total);
 */
 int main(int argc, char *argv[])
 { 
-    int RANK_MASTER = 0;
+    int MASTER = 0;
     int n, i, split_per_procs, start, end;
     int numprocs, rank, namelen, dest=0, tag=0, source;
 	int BUFSIZE = 50;
@@ -25,16 +25,30 @@ int main(int argc, char *argv[])
 	MPI_Get_processor_name(processor_name, &namelen);
     split_per_procs = n/numprocs; 
 
+    // Splitting Method
     start = (rank * split_per_procs) + 1;
     end = (rank+1) * split_per_procs;  
-
+    
     for (i=start; i<=end; i++)
         local_sum += 1.0/i;
     MPI_Reduce(&local_sum, &global_sum, 1, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
-	if (rank == 0){
-        printf("\nThe sum of the reciprocal from 1 to %d is %f.\n", n, global_sum);
+	if (rank == MASTER){
+        printf("[Splitting] The sum of the reciprocal from 1 to %d is %f.\n", n, global_sum);
     } 
 	
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    // Intersperse
+    local_sum = 0;
+    global_sum = 0;
+    for (int i=rank+1; i<=n; i=i+numprocs){
+        local_sum += 1.0/i;
+    }
+    MPI_Reduce(&local_sum, &global_sum, 1, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
+    if (rank == MASTER){
+        printf("[Intersperse] The sum of the reciprocal from 1 to %d is %f.\n", n, global_sum);
+    } 
+
     MPI_Finalize();
 
 }
