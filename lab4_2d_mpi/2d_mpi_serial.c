48ec9a977c3ef22399e0da0e9e5b2eaf244011fa
jxwleong <jason.xie.wei.leong@gmail.com>
2022-11-27 18:46:53 +0800

    Added lab4 serial type code

---
+++ lab4_2d_mpi/2d_mpi_serial.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#define MAXPIXEL 256
+#define TEMPFILE "temp.pgm"
+
+int xsize, ysize;
+
+int f(int x, int y)
+{ 
+    double divisor = (M_PI*xsize/60);
+    double zx = (double)x/divisor;
+    double zy = (double)y/divisor;
+    return (sin(zx)+cos(zy))*(MAXPIXEL/4)+(MAXPIXEL/2);
+}
+int main(int argc, char*argv[])
+{ 
+    int x, y;
+    int **picture;
+    FILE *outfile;
+    printf("How wide and tall should the picture be? ");fflush(stdout);
+    scanf("%d %d", &xsize, &ysize);
+    picture =(int**)malloc(sizeof(int*)*ysize);
+    for (y=0; y<ysize; y++)
+    { 
+        picture[y] = (int*)malloc(sizeof(int)*xsize);
+        for (x=0; x<xsize; x++)
+            picture[y][x] = f(x,y);
+    }
+    outfile=fopen(TEMPFILE,"w");
+    fprintf(outfile,"P2\n%d %d %d\n",xsize, ysize, MAXPIXEL);
+    for(y=0; y<ysize; y++)
+    for (x=0; x<xsize; x++)
+    fprintf(outfile,"%d\n", picture[y][x]);
+    fclose(outfile);
+    system("display "TEMPFILE);
+}
